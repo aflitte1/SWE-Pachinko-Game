d889fa2b1c6f03352f4b319109d636f3e7436ee5
Christian <christian.lostoski@gmail.com>
2023-03-21 11:50:44 -0400

    Added Player file, working on implementation

--- src/components/game_phases.py
+++ src/components/game_phases.py
@@ -3,6 +3,13 @@ import pymunk
 import pygame
 from Game import *
 from components.Backend import *
+from components.player import *
+
+# Sprite Setup
+P1 = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
 
 
 def main_menu_phase():
@@ -17,18 +24,24 @@ def level_one():
     # The background image will eventually become a global variable dependent on cosmetics
     BackGround.IMAGE = Background('assets/1.jpg', [0, 0], 1.0)
     space.gravity = (0, 800)
+    P1.update()
+    P1.draw(Screen)
+
 
 def level_two():
     BackGround.IMAGE = Background('assets/2.jpg', [0, 0], 1.5)
     space.gravity = (0, 500)
 
+
 def level_three():
     BackGround.IMAGE = Background('assets/3.jpg', [0, 0], 1.5)
     space.gravity = (0, 100)
 
+
 def level_four():
     BackGround.IMAGE = Background('assets/4.jpg', [0, 0], 1.5)
     space.gravity = (0, 450)
+
 
 def cos_menu():
     BackGround.IMAGE = Background('assets/cosmetic.jpg', [0, 0], 1.5)
---
+++ src/components/player.py
@@ -0,0 +1,50 @@
+import pygame
+from pygame.locals import *
+
+vec = pygame.math.Vector2
+ACC = 0.5
+FRIC = -0.10
+HIGHT = 800
+WIDTH = 800
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('assets/bucket.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.pos = vec((180, 550))
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.player_position = vec(0, 0)
+
+    def move(self):
+        self.acc = vec(0, 0)
+
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[K_LEFT] or pressed_keys[K_a]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT] or pressed_keys[K_d]:
+            self.acc.x = ACC
+        
+        self.acc.x += self.vel.x * FRIC
+        self.acc.y += self.vel.y * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        self.player_position = self.pos.copy()
+
+        if self.pos.x > WIDTH:
+            self.pos.x = WIDTH
+        if self.pos.x < 0:
+            self.pos.x = 0
+        if self.pos.y > HIGHT:
+            self.pos.y = HIGHT
+        if self.pos.y < 0:
+            self.pos.y = 0
+
+        self.rect.center = self.pos
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
