af273228a4f5b55f87125f09e53e0f4a52c2baa4
Christian Lostoski <norexus1@gmail.com>
2023-04-18 16:53:35 -0400

    Implemented Score show at game end

--- src/Game.py
+++ src/Game.py
@@ -25,16 +25,27 @@ def update_game_display():
     Clock.tick(120)
     Space.step(1/50)  # Updating time for physics sim
 
-def game_over():
+
+def game_over(score, total):
     GAME_MOUSE_POS = pygame.mouse.get_pos()
     GAME_TEXT = bnd.get_font(50).render("GAME OVER", True, "#b68f40")
     GAME_RECT = GAME_TEXT.get_rect(center=(400, 100))
 
-    PLAY_BUTTON = bnd.Button(image=pygame.image.load("assets/Play Rect.png"), pos=(400, 400), 
-                        text_input="PLAY AGAIN", font=bnd.get_font(35), base_color="#d7fcd4", hovering_color="White")
-    MENU_BUTTON = bnd.Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(400, 550), 
-                        text_input="MAIN MENU", font=bnd.get_font(35), base_color="#d7fcd4", hovering_color="White")
+    SCORE_WRD = bnd.get_font(50).render("SCORE", True, "#f5da0c")
+    SCORE_WRD_RECT = SCORE_WRD.get_rect(center=(400, 250))
+
+    SCORE_STR = str(score) + "/" + str(total)
+    SCORE_TEXT = bnd.get_font(50).render(SCORE_STR, True, "#f5da0c")
+    SCORE_RECT = SCORE_TEXT.get_rect(center=(400, 300))
+
     Screen.blit(GAME_TEXT, GAME_RECT)
+    Screen.blit(SCORE_WRD, SCORE_WRD_RECT)
+    Screen.blit(SCORE_TEXT, SCORE_RECT)
+
+    PLAY_BUTTON = bnd.Button(image=pygame.image.load("assets/Play Rect.png"), pos=(400, 400),
+                             text_input="PLAY AGAIN", font=bnd.get_font(35), base_color="#d7fcd4", hovering_color="White")
+    MENU_BUTTON = bnd.Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(400, 550),
+                             text_input="MAIN MENU", font=bnd.get_font(35), base_color="#d7fcd4", hovering_color="White")
 
     for button in [PLAY_BUTTON, MENU_BUTTON]:
         button.changeColor(GAME_MOUSE_POS)
@@ -52,6 +63,7 @@ def game_over():
                 return True
     return False
 
+
 def main():
     Balls = []
     Pegs = []
@@ -59,7 +71,7 @@ def main():
     ball_count = 0
     ball_pos = 0
     level_start = False
-    collision_count = 0
+    score = 0
     bnd.Default_Cosmetics.state = True
     bnd.create_borders(Space)
     while True:
@@ -93,8 +105,7 @@ def main():
                         ball_count += 1
 
                 if bnd.collide(P1, Balls):
-                    collision_count += 1
-                    print("Collision ", collision_count)
+                    score += 1
 
             case bnd.GameStatus.LEVEL_2:
                 GamePhases.level_two()
@@ -113,17 +124,16 @@ def main():
 
                 if ball_count <= ball_max:
                     spawn_ball = np.random.randint(0, 50)
-                    if spawn_ball == 0:     
+                    if spawn_ball == 0:
                         if ball_count % 10 == 0:
-                            ball_pos += 180 
+                            ball_pos += 180
                         ball_sprite = bnd.Ball(ball_pos, Space, 43, 1)
                         Balls.append(ball_sprite)
                         all_balls.add(ball_sprite)
                         ball_count += 1
 
                 if bnd.collide(P1, Balls):
-                    collision_count += 1
-                    print("Collision ", collision_count)
+                    score += 1
 
             case bnd.GameStatus.LEVEL_4:
                 Space.gravity = (0, 300)
@@ -139,17 +149,16 @@ def main():
 
                 if ball_count <= ball_max:
                     spawn_ball = np.random.randint(0, 50)
-                    if spawn_ball == 0:     
+                    if spawn_ball == 0:
                         if ball_count % 10 == 0:
-                            ball_pos += 180 
+                            ball_pos += 180
                         ball_sprite = bnd.Ball(ball_pos, Space, 43, 1)
                         Balls.append(ball_sprite)
                         all_balls.add(ball_sprite)
                         ball_count += 1
 
                 if bnd.collide(P1, Balls):
-                    collision_count += 1
-                    print("Collision ", collision_count)
+                    score += 1
 
             case bnd.GameStatus.COS_MENU:
                 GamePhases.cos_menu(Screen)
@@ -157,19 +166,20 @@ def main():
         if bnd.GlobalState.GAME_STATE != bnd.GameStatus.MAIN_MENU:
             if bnd.GlobalState.GAME_STATE != bnd.GameStatus.COS_MENU:
                 Screen.fill((217, 217, 217))
-                Screen.blit(bnd.BackGround.IMAGE.image, bnd.BackGround.IMAGE.rect)
+                Screen.blit(bnd.BackGround.IMAGE.image,
+                            bnd.BackGround.IMAGE.rect)
                 for i in range(len(Balls)):
                     Balls[i].draw(Screen)
                 bnd.draw_peg(Screen, Pegs)
                 P1.move()
                 P1.draw(Screen)
-                if(ball_count == ball_max+1) & (len(Balls) == 0):
-                    if(game_over()):
+                if (ball_count == ball_max+1) & (len(Balls) == 0):
+                    if (game_over(score, ball_count)):
                         Pegs.clear()
                         ball_count = 0
                         ball_pos = 0
                         level_start = False
-                        collision_count = 0 
+                        score = 0
 
                 for ball in Balls:
                     if (bnd.delete_ball(ball.phys.body.position.y)):
